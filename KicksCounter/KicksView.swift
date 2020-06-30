14528d03f0fd3b975cf53d2f3e87a4b94bd44960
Ryan Mason-Davies <iotize@icloud.com>
2020-06-30 21:30:28 +0100

    Update Kicks Today UI

--- KicksCounter/KicksView.swift
+++ KicksCounter/KicksView.swift
@@ -2,42 +2,42 @@ import SwiftUI
 
 struct KicksView: View {
     @Binding var appState: AppState
-
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Kicks Today")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .layoutPriority(0)
-
-                VStack {
-                    ZStack {
-                        Circle()
-                            .foregroundColor(.accentColor)
-                        Text("\(appState.kicks.count)")
-                            .font(.largeTitle)
-                    }
-                    .padding(.all, 32)
-                    .layoutPriority(2)
-
-                    ZStack {
-                        Button(action: {
-                            self.appState.kicks.append(Kick())
-                        }) {
-                            Image(systemName: "plus.circle")
+            ScrollView(.vertical, showsIndicators: true) {
+                VStack(alignment: .center, spacing: 32) {
+                    Text("Kicks Today")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    VStack(alignment: .center, spacing: 32) {
+                        ZStack {
+                            Circle()
                                 .foregroundColor(.accentColor)
+                            Text("\(appState.kicks.count)")
                                 .font(.largeTitle)
+                                .padding(40)
+                        }
+                        
+                        ZStack {
+                            Button(action: {
+                                self.appState.kicks.append(Kick())
+                            }) {
+                                Image(systemName: "plus.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: 50, alignment: .center)
+                                    .foregroundColor(.accentColor)
+                                    .font(.largeTitle)
+                            }
                         }
                     }
-                }
-                .layoutPriority(0)
-
-                Spacer()
-                    .layoutPriority(0)
-                VStack(alignment: .leading, spacing: 4) {
-                    ForEach(SummaryTimeframe.allCases, id: \.self) {
-                        KickSummaryCard(timeframe: $0)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        ForEach(SummaryTimeframe.allCases, id: \.self) {
+                            KickSummaryCard(timeframe: $0)
+                        }
                     }
                 }
             }
