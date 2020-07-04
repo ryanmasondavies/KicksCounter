8c6a8c767c79139bb54c2c75617de1e512bcb673
Ryan Mason-Davies <iotize@icloud.com>
2020-07-04 13:39:09 +0100

    Add kicks with date only

--- KicksCounter/Model/Kick.swift
+++ KicksCounter/Model/Kick.swift
@@ -2,8 +2,14 @@ import Foundation
 
 struct Kick: Codable {
     let date: Date
+    var includesTime = true
 
     init() {
         self.date = Date()
+    }
+
+    init(date: Date, includesTime: Bool) {
+        self.date = date
+        self.includesTime = includesTime
     }
 }
