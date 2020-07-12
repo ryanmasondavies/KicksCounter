bdf079267cb808905ced1b682f184c57fb938395
Ryan Mason-Davies <iotize@icloud.com>
2020-07-12 20:56:49 +0100

    Add basic complication with static number

--- Kicks Extension/ComplicationController.swift
+++ Kicks Extension/ComplicationController.swift
@@ -23,12 +23,64 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
+        guard let template: CLKComplicationTemplate = {
+            switch(complication.family) {
+            case .modularSmall:
+                let template = CLKComplicationTemplateModularSmallSimpleText()
+                template.textProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .modularLarge:
+                let template = CLKComplicationTemplateModularLargeStandardBody()
+                template.headerTextProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .circularSmall:
+                let template = CLKComplicationTemplateCircularSmallSimpleText()
+                template.textProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .utilitarianSmall:
+                let template = CLKComplicationTemplateUtilitarianSmallFlat()
+                template.textProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .utilitarianSmallFlat:
+                let template = CLKComplicationTemplateUtilitarianSmallFlat()
+                template.textProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .utilitarianLarge:
+                let template = CLKComplicationTemplateUtilitarianLargeFlat()
+                template.textProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .extraLarge:
+                let template = CLKComplicationTemplateExtraLargeSimpleText()
+                template.textProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .graphicBezel:
+                let template = CLKComplicationTemplateGraphicBezelCircularText()
+                template.textProvider = CLKSimpleTextProvider(text: "10")
+                return template
+            case .graphicCorner:
+                let template = CLKComplicationTemplateGraphicCornerStackText()
+                template.innerTextProvider = CLKSimpleTextProvider(text: "10")
+                template.outerTextProvider = CLKSimpleTextProvider(text: "")
+                return template
+            case .graphicCircular:
+                let template = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+                template.centerTextProvider = CLKSimpleTextProvider(text: "10")
+                template.gaugeProvider = CLKSimpleGaugeProvider(style: .ring, gaugeColor: .white, fillFraction: 1)
+                return template
+            case .graphicRectangular:
+                let template = CLKComplicationTemplateGraphicRectangularStandardBody()
+                template.headerTextProvider = CLKTextProvider(format: "10")
+                return template
+            default:
+                return nil
+            }
+            }() else {
+                return handler(nil)
+        }
+        handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
     }
-    
+
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries prior to the given date
         handler(nil)
     }
     
